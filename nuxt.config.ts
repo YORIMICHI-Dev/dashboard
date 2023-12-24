118fc29e62355d7818df7616d58541a3206b78c6
yorimichi <yorimichi.dev@gmail.com>
2023-12-25 01:14:50 +0900

    feat: :memo: Add config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,28 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
+import vuetify, { transformAssetUrls } from 'vite-plugin-vuetify';
+
 export default defineNuxtConfig({
-  devtools: { enabled: true }
-})
+  ssr: false,
+  build: {
+    transpile: ['vuetify'],
+  },
+  devtools: { enabled: true },
+  modules: [
+    // pinia
+    '@pinia/nuxt',
+    // vuetify setting
+    (_options, nuxt) => {
+      nuxt.hooks.hook('vite:extendConfig', (config) => {
+        // @ts-expect-error: vuetifyのdefault設定
+        config.plugins.push(vuetify({ autoImport: true }));
+      });
+    },
+  ],
+  vite: {
+    vue: {
+      template: {
+        transformAssetUrls,
+      },
+    },
+  },
+});
