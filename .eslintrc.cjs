1158508ce63408b871aac936e5f1a800e0cc68a2
yorimichi <yorimichi.dev@gmail.com>
2023-12-25 01:14:27 +0900

    style: :art: add eslint prettier

---
+++ .eslintrc.cjs
@@ -0,0 +1,38 @@
+module.exports = {
+    "env": {
+        "browser": true,
+        "es2021": true
+    },
+    "extends": [
+        "eslint:recommended",
+        "plugin:@typescript-eslint/recommended",
+        "plugin:vue/vue3-essential"
+    ],
+    "overrides": [
+        {
+            "env": {
+                "node": true
+            },
+            "files": [
+                ".eslintrc.{js,cjs}"
+            ],
+            "parserOptions": {
+                "sourceType": "script"
+            }
+        }
+    ],
+    "parserOptions": {
+        "ecmaVersion": "latest",
+        "parser": "@typescript-eslint/parser",
+        "sourceType": "module"
+    },
+    "plugins": [
+        "@typescript-eslint",
+        "vue"
+    ],
+    "rules": {
+        "no-undef": "off", // 自動import採用
+        "vue/multi-word-component-names": "off", // componentの命名は特に複数単語でなくてよい
+        "@typescript-eslint/no-explicit-any": "off" // any型は都度確認
+    }
+}
