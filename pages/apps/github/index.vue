a1d577e8bbf0422a558fc1f2d09265cf58429559
yorimichi <yorimichi.dev@gmail.com>
2023-12-27 00:31:41 +0900

    feat: :memo: Add New page

---
+++ pages/apps/github/index.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+const TITLE = 'Github Repository';
+useHead({
+  title: TITLE,
+});
+
+const breadcrumbs = ref([
+  {
+    text: 'Apps',
+    disabled: false,
+    href: '/',
+  },
+  {
+    text: TITLE,
+    disabled: true,
+    href: '',
+  },
+]);
+</script>
+
+<template>
+  <!-- Breadcrumb -->
+  <SharedBaseBreadCrumb :title="TITLE" :breadcrumbs="breadcrumbs" />
+</template>
---
+++ pages/index.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+const TITLE = 'Home';
+useHead({
+  title: TITLE,
+});
+
+const breadcrumbs = ref([
+  {
+    text: 'Dashboard',
+    disabled: false,
+    href: '/',
+  },
+  {
+    text: TITLE,
+    disabled: true,
+    href: '',
+  },
+]);
+</script>
+
+<template>
+  <!-- Breadcrumb -->
+  <SharedBaseBreadCrumb :title="TITLE" :breadcrumbs="breadcrumbs" />
+</template>
