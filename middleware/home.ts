46f2cf905f1b135c0b0bc3b1a5713a096d32dcfa
yorimichi <yorimichi.dev@gmail.com>
2023-12-29 11:44:06 +0900

    fix: :bug: fixed

--- middleware/home.ts
+++ middleware/home.ts
@@ -2,4 +2,9 @@ export default defineNuxtRouteMiddleware((to, from) => {
     if (to.path === '/') {
         return navigateTo('/dashboard/profile', {redirectCode: 301})
     }
+
+    // For vercel
+    if (to.path === '/dashboard-daisuke.vercel.app') {
+        return navigateTo('/dashboard-daisuke.vercel.app/dashboard/profile', {redirectCode: 301})
+    }
 })
