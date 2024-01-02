f14817fd68aa094b8c16a84a080889a2a4b49eb0
yorimichi <yorimichi.dev@gmail.com>
2024-01-02 18:37:19 +0900

    fix:

--- components/logo/LogoDark.vue
+++ components/logo/LogoDark.vue
@@ -3,7 +3,10 @@
 <template>
   <div class="logo">
     <NuxtLink to="/" class="d-flex gap-2">
-      <img width="32" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/nuxtjs/nuxtjs-original.svg" />
+      <img
+        width="32"
+        src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/nuxtjs/nuxtjs-original.svg"
+      />
       <h3 class="h3">Dashboard</h3>
     </NuxtLink>
   </div>
